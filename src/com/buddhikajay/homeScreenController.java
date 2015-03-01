3815d0f41c7882113398587c9c3e3cf4be1270e7
buddhika-jay <buddhika.12@cse.mrt.ac.lk>
2015-03-01 14:51:33 +0530

    SqliteDatabaseTesst|Two tests passed

--- src/com/buddhikajay/homeScreenController.java
+++ src/com/buddhikajay/homeScreenController.java
@@ -1,41 +1,21 @@
 package com.buddhikajay;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 /**
  * Created by Buddhika Jayawardhana on 27/02/2015.
+ * JavaFx
  */
 public class homeScreenController extends Application{
-
-    @FXML
-    public Button deleteButton;
-    @FXML
-    public Button addNewButton;
-    @FXML
-    private Button searchButton;
-    @FXML
-    private Button editButton;
-    @FXML
-    public TableView reportTable;
-    @FXML
-    public TableColumn personColumn;
-
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("homeScreen.fxml"));
         primaryStage.setTitle("Virtual Credit");
         primaryStage.setScene(new Scene(root, 700, 500));
         primaryStage.show();
-
     }
-
 }
 
