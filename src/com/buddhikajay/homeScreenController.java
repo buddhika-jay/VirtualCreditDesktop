f26130ada3ef5a58334f13f5eb7e9da67888661f
buddhika-jay <buddhika.12@cse.mrt.ac.lk>
2015-03-01 16:36:26 +0530

    colums defined in start method

--- src/com/buddhikajay/homeScreenController.java
+++ src/com/buddhikajay/homeScreenController.java
@@ -3,6 +3,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 /**
@@ -10,12 +12,34 @@ import javafx.stage.Stage;
  * JavaFx
  */
 public class homeScreenController extends Application{
+    public TableView reportTable;
+    public TableColumn idColumn;
+    public TableColumn typeColumn;
+    public TableColumn amountColumn;
+    public TableColumn personColumn;
+    public TableColumn dateColumn;
+    public TableColumn descriptionColumn;
+    public TableColumn resolvedColumn;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("homeScreen.fxml"));
         primaryStage.setTitle("Virtual Credit");
         primaryStage.setScene(new Scene(root, 700, 500));
         primaryStage.show();
+
+        //idColumn.setCellValueFactory(new PropertyValueFactory<TableTransaction, String>("id"));
+        /*
+        typeColumn.setCellValueFactory(new PropertyValueFactory<TableTransaction,String>("type"));
+        amountColumn.setCellValueFactory(new PropertyValueFactory<TableTransaction, String>("amount"));
+        personColumn.setCellValueFactory(new PropertyValueFactory<TableTransaction, String>("person"));
+        dateColumn.setCellValueFactory(new PropertyValueFactory<TableTransaction, String>("date"));
+        descriptionColumn.setCellValueFactory(new PropertyValueFactory<TableTransaction, String>("description"));
+        resolvedColumn.setCellValueFactory(new PropertyValueFactory<TableTransaction, String>("resolved"));
+       */
+    }
+    public static void main(String args[]){
+        Application.launch();
     }
 }
 
